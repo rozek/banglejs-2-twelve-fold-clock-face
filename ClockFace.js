a4bfadd3f7be0d24fcf84bcf5bd6dc72e69d222e
Andreas Rozek <A.Rozek@gmx.de>
2022-01-08 18:33:12 +0100

    Create ClockFace.js

---
+++ ClockFace.js
@@ -0,0 +1,33 @@
+(function () {
+  exports.draw = function draw (Settings, CenterX, CenterY, outerRadius) {
+    let sin = Math.sin, cos = Math.cos;
+    let twoPi = 2*Math.PI;
+
+    g.setColor(Settings.Foreground === 'Theme' ? Theme.fg : Settings.Foreground || '#000000');
+
+    let withDots = Setings.withDots;
+    if (withDots) {
+      for (let i = 0; i < 60; i++) {
+        let Phi = i * twoPi/60;
+
+        let x = CenterX + outerRadius * sin(Phi);
+        let y = CenterY - outerRadius * cos(Phi);
+
+        g.fillCircle(x,y, 1);
+      }
+    }
+
+    g.setFont('Vector', 22);
+    g.setFontAlign(0,0);
+
+    let innerRadius = (withDots ? outerRadius * 0.9 - 10 : outerRadius);
+    for (let i = 0; i < 12; i++) {
+      let Phi = i * twoPi/12;
+
+      let x = CenterX + innerRadius * sin(Phi);
+      let y = CenterY - innerRadius * cos(Phi);
+
+      g.drawString(i == 0 ? '12' : '' + i, x,y);
+    }
+  };
+})();
